5bcd10e78eb1c8e65440894797a42f9171177049
Yruikk <49941992+Yruikk@users.noreply.github.com>
2022-12-18 18:55:54 +0800

    Delete Fig4 directory

--- Fig4/L2Loss.m
+++
@@ -1,5 +0,0 @@
-function loss = L2Loss(pred,target)
-%L2LOSS
-loss = (pred-target)^2;
-end
-
--- Fig4/calsim.m
+++
@@ -1,3 +0,0 @@
-function sim = calsim(vec_1,vec_2)
-sim = (vec_1'*vec_2)/(norm(vec_1,2)*norm(vec_2,2));
-end
